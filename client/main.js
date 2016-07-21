133e8fbfba7c31adb85b061127fac27913afa385
walter <wutong@ihealthlabs.com.cn>
2016-07-21 16:31:51 +0800

    react helloworld

--- client/main.js
+++ client/main.js
@@ -1,22 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+// import HelloWorld from './HelloWorld.js';
 
-import './main.html';
+const HelloWorld = () => (
+  <div>
+    <p>HelloWorld</p>
+  </div>
+)
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  render(<HelloWorld />, document.getElementById('mount'));
 });
